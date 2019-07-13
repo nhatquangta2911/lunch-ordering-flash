c1caacfa7ae0ed8c3a5539901b40765eaae1c3a3
NhatQuangTa <nhatquangta2911@gmail.com>
2019-07-13 15:18:36 +0700

    Change Port, Eliminate Previous Route

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace CourseApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:2911");
     }
 }
